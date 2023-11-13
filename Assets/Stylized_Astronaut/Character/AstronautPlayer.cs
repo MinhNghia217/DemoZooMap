a664c31e75e8296609207a90a9c53d75f96542e3
MinhNghia217 <trantruongminhnghia12345@gmail.com>
2023-11-13 18:10:01 +0700

    push

---
+++ Assets/Stylized_Astronaut/Character/AstronautPlayer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AstronautPlayer
+{
+
+	public class AstronautPlayer : MonoBehaviour {
+
+		private Animator anim;
+		private CharacterController controller;
+
+		public float speed = 600.0f;
+		public float turnSpeed = 400.0f;
+		private Vector3 moveDirection = Vector3.zero;
+		public float gravity = 20.0f;
+
+		void Start () {
+			controller = GetComponent <CharacterController>();
+			anim = gameObject.GetComponentInChildren<Animator>();
+		}
+
+        void Update()
+        {
+            // Kiểm tra nút W được nhấn hay không
+            bool isMoving = Input.GetKey("w");
+
+            // Thiết lập giá trị AnimationPar trong Animator dựa trên trạng thái di chuyển
+            anim.SetInteger("AnimationPar", isMoving ? 1 : 0);
+
+            // Kiểm tra player đang ở trên mặt đất
+            if (controller.isGrounded)
+            {
+                // Tính toán vector di chuyển dựa trên đầu vào từ trục dọc (Vertical)
+                moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
+            }
+
+            // Tính toán góc quay từ đầu vào trục ngang (Horizontal)
+            float turn = Input.GetAxis("Horizontal");
+            transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+
+            // Di chuyển player
+            controller.Move(moveDirection * Time.deltaTime);
+
+            // Áp dụng trọng lực
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Stylized_Astronaut/Character/AstronautThirdPersonCamera.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AstronautThirdPersonCamera
+{
+    public class AstronautThirdPersonCamera : MonoBehaviour
+    {
+        private const float Y_ANGLE_MIN = 0.0f;
+        private const float Y_ANGLE_MAX = 50.0f;
+
+        public Transform lookAt;
+        public Transform camTransform;
+        public float distance = 5.0f;
+        public float followSpeed = 5.0f;
+
+        private float currentX = 0.0f;
+        private float currentY = 45.0f;
+        private float sensitivityX = 5.0f;
+        private float sensitivityY = 5.0f;
+        private float smoothTime = 0.1f;
+        private Vector3 velocity;
+
+        private Vector3 lastLookAtPosition;
+
+        private void Start()
+        {
+            camTransform = transform;
+            lastLookAtPosition = lookAt.position;
+        }
+
+        private void Update()
+        {
+            currentX += Input.GetAxis("Mouse X") * sensitivityX;
+            currentY -= Input.GetAxis("Mouse Y") * sensitivityY;
+
+            currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+        }
+
+        private void LateUpdate()
+        {
+            Vector3 dir = new Vector3(0, 0, -distance);
+            Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+            Vector3 targetPosition = lookAt.position + rotation * dir;
+
+            // Tính toán hướng di chuyển của nhân vật
+            Vector3 moveDirection = lookAt.position - lastLookAtPosition;
+
+            // Áp dụng hướng di chuyển vào vị trí của camera
+            targetPosition += moveDirection * followSpeed;
+
+            lastLookAtPosition = lookAt.position;
+
+            camTransform.position = Vector3.SmoothDamp(camTransform.position, targetPosition, ref velocity, smoothTime);
+            camTransform.LookAt(lookAt.position);
+        }
+    }
+}
